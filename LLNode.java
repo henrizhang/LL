cfec7ae24a51cfdeccd231296fbaf19f96f52448
Henry <hzhang5@stuy.edu>
2016-03-15 20:17:11 -0400

    Part 1

--- LLNode.java
+++ LLNode.java
@@ -1,2 +1,37 @@
+/* Henry Zhang
+ ;APCS2 pd9
+ ;HW #12a: We Got a Big Ol’ Convoy...
+ ;2016-03-14
+ */
+
 public class LLNode{
+    private String _cargo;
+    private LLNode _next;
+    //instance vars
+    
+    public LLNode(String cargo, LLNode next) {
+        _cargo=cargo;
+        _next=next;
+    }
+    //constructor
+    public void setCargo(String cargo){
+        _cargo=cargo;
+        
+    }
+    //node's data mutator
+    public String getCargo(){
+        return _cargo;
+    }
+    //node's data accessor
+    public void setLinkage(LLNode next){
+        _next=next;
+    }
+    //change linkage
+    public String getStringLinkage(){
+        return _next.getCargo();
+    }
+    public LLNode getLinkage(){
+        return _next;
+    }
+    //linked node accessor
 }
--- LList.java
+++ LList.java
@@ -1,2 +1,56 @@
-public class LList{
+/* Henry Zhang
+ ;APCS2 pd9
+ ;HW #12b: Truckin’ Through the Night
+ ;2016-03-14
+ */
+
+
+public class LList implements List{
+    private LLNode head;
+    public LList(){
+        head=null;
+    }
+    public boolean add(String x){
+        head= new LLNode(x, head);
+	return true;
+    }
+    public String get(int i){
+        LLNode temp=head;
+        int x=0;
+        while(x<i){
+            temp=head.getLinkage();
+            x++;
+        }
+        return temp.getCargo();
+        
+    }
+    
+    public String set(int i, String x){
+        int p=0;
+        LLNode temp=head;
+        while(p<i){
+            temp=head.getLinkage();
+            p++;
+        }
+       
+	LLNode end= new LLNode(x, head.getLinkage());
+        head.setLinkage(end);
+	return head.getCargo();
+	
+    }
+    public int size(){
+        int x=0;
+	LLNode temp=head;
+        while(1==1){
+            if (head.getCargo()==null){
+                return x;
+            }
+            else{
+                temp=head.getLinkage();
+                x++;
+            }
+        }
+        
+    }
+    
 }
